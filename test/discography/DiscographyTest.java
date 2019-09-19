39f558679f80bed7f67cba57231911ba11d113ff
Morten Olsen <morten222@hotmail.com>
2019-09-19 20:16:11 +0200

    TC5: Added testCreateDiscography

--- test/discography/DiscographyTest.java
+++ test/discography/DiscographyTest.java
@@ -1,7 +1,15 @@
 package discography;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Month;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiscographyTest {
-
+    @Test
+    public void testCreateDiscography() {
+    }
 }
