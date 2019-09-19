ba8e2279c4fee5ec265401c4251de97f2cf976df
Morten Olsen <morten222@hotmail.com>
2019-09-19 20:17:41 +0200

    TC5: Test for getArtistName() added (RED BAR)

--- test/discography/DiscographyTest.java
+++ test/discography/DiscographyTest.java
@@ -2,14 +2,12 @@ package discography;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.Month;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiscographyTest {
     @Test
     public void testCreateDiscography() {
+        Discography discography = new Discography("Future Artist");
+        assertEquals("Future Artist", discography.getArtistName());
     }
 }
