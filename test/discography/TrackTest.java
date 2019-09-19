85d1279106ba02abbd2c4feb2b0f5455ec9e8aa5
Morten Olsen <40779401+Min222@users.noreply.github.com>
2019-09-19 14:07:05 +0200

    testCreateTrack added (RED BAR, cannot compile) Test list added

--- test/discography/TrackTest.java
+++ test/discography/TrackTest.java
@@ -5,4 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrackTest {
+    @Test
+    public void testCreateTrack() {
+        Track track = new Track("title");
+        String title = track.getTitle();
+        assertEquals(title, "title");
+    }
 }
