b9d23d13d25e141bb805d11d75761858b44a5ab9
Morten Olsen <morten222@hotmail.com>
2019-09-19 19:18:40 +0200

    TC3: Test for getTitle() added (RED BAR)

--- test/discography/RecordTest.java
+++ test/discography/RecordTest.java
@@ -7,5 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RecordTest {
     @Test
     public void testCreateRecord() {
+        Record record = new Record("Under Production");
+        assertEquals("Under Production", record.getTitle());
+
     }
 }
