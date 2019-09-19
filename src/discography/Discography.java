40c849a1a350b1f5324bd90c2a0685de07df4495
Morten Olsen <morten222@hotmail.com>
2019-09-19 20:18:36 +0200

    TC5: Test for getArtistName() passed (GREEN BAR)

---
+++ src/discography/Discography.java
@@ -0,0 +1,10 @@
+package discography;
+
+public class Discography {
+    public Discography(String artistName) {
+    }
+
+    public String getArtistName() {
+        return "Future Artist";
+    }
+}
