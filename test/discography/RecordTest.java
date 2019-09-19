208658087aac7c28930aef4e620cfe16275eeec6
Morten Olsen <morten222@hotmail.com>
2019-09-19 19:22:21 +0200

    TC3: Test for getReleaseDate() added (RED BAR)

--- test/discography/RecordTest.java
+++ test/discography/RecordTest.java
@@ -2,6 +2,9 @@ package discography;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RecordTest {
@@ -9,6 +12,6 @@ public class RecordTest {
     public void testCreateRecord() {
         Record record = new Record("Under Production");
         assertEquals("Under Production", record.getTitle());
-
+        assertEquals(LocalDate.of(2074, Month.OCTOBER, 6), record.getReleaseDate());
     }
 }
